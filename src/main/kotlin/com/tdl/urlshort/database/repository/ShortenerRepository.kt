26427d8b1e8b515c67426dee7a8ad388baf01d02
Fedacking <fgauna@fi.uba.ar>
2022-05-30 16:37:08 -0300

    Add: MongoDB Connection

--- src/main/kotlin/com/tdl/urlshort/database/repository/ShortenerRepository.kt
+++ src/main/kotlin/com/tdl/urlshort/database/repository/ShortenerRepository.kt
@@ -1,4 +1,8 @@
 package com.tdl.urlshort.database.repository
+import javax.validation.Valid
 
 interface ShortenerRepository {
+    fun list(): List<DbUrlEntry>
+
+    fun save(@Valid entry: DbUrlEntry)
 }
--- src/test/kotlin/com/tdl/urlshort/UrlShortenerTest.kt
+++ src/test/kotlin/com/tdl/urlshort/UrlShortenerTest.kt
@@ -1,19 +1,29 @@
 package com.tdl.urlshort
+import com.tdl.urlshort.database.repository.DbUrlEntry
+import com.tdl.urlshort.database.repository.ShortenerRepository
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import jakarta.inject.Inject
 
-@MicronautTest
+@MicronautTest(environments = ["prod"])
 class UrlShortenerTest {
 
     @Inject
     lateinit var application: EmbeddedApplication<*>
+
+    @Inject
+    lateinit var repo: ShortenerRepository
 
     @Test
     fun testItWorks() {
         Assertions.assertTrue(application.isRunning)
     }
 
+    @Test
+    fun testMongoWorks() {
+        repo.save(DbUrlEntry("dasd", "test"))
+        Assertions.assertTrue(repo.list().isEmpty())
+    }
 }
