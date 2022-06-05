c7539d68a2b1f14c6db684c55336c8d15535787f
Katta <gkatta@fi.uba.ar>
2022-06-05 15:41:21 -0300

    Fix: Refactored REDIRECT_URL contract

--- src/main/kotlin/com/tdl/urlshort/service/ShorteningService.kt
+++ src/main/kotlin/com/tdl/urlshort/service/ShorteningService.kt
@@ -3,13 +3,12 @@ package com.tdl.urlshort.service
 import com.tdl.urlshort.database.model.URL
 import com.tdl.urlshort.dtos.ApiResponse
 import com.tdl.urlshort.dtos.LongURL
-import com.tdl.urlshort.dtos.ShortURL
 import com.tdl.urlshort.dtos.URLMetrics
 
 interface ShorteningService {
 
     fun shortenURL(url:LongURL) : URL
-    fun redirectURL(url:ShortURL) : ApiResponse
+    fun redirectURL(hash:String) : ApiResponse
     fun getUsageMetrics(hash:String) : URLMetrics
 
 }
--- src/main/kotlin/com/tdl/urlshort/service/URLShorteningService.kt
+++ src/main/kotlin/com/tdl/urlshort/service/URLShorteningService.kt
@@ -4,7 +4,6 @@ import com.tdl.urlshort.database.model.URL
 import com.tdl.urlshort.database.repository.URLRepository
 import com.tdl.urlshort.dtos.ApiResponse
 import com.tdl.urlshort.dtos.LongURL
-import com.tdl.urlshort.dtos.ShortURL
 import com.tdl.urlshort.dtos.URLMetrics
 import com.tdl.urlshort.util.HashUtils
 import com.tdl.urlshort.util.URLUtils
@@ -17,7 +16,7 @@ class URLShorteningService(private val repository: URLRepository,
 {
     override fun shortenURL(url : LongURL) : URL = TODO("Not yet implemented")
 
-    override fun redirectURL(url : ShortURL) : ApiResponse = TODO("Not yet implemented")
+    override fun redirectURL(hash:String) : ApiResponse = TODO("Not yet implemented")
 
     override fun getUsageMetrics(hash : String) : URLMetrics = TODO("Not yet implemented")
 }
